88a813e2164df28526428a746c44570e78c40db2
Carl Holmqvist <caholmq@kth.se>
2024-09-20 18:10:39 +0200

    added further logic for monster distraction, like showing effective range before operator places it in the maze, limiting the amount of uses and making sure  it can only be placed inside the maze

--- Assets/Scripts/PlaceDistraction.cs
+++ Assets/Scripts/PlaceDistraction.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlaceDistraction : MonoBehaviour
 {
     [SerializeField] public Camera topDownView;
     [SerializeField] public GameObject distraction;
-
+    [SerializeField] public GameObject distractionIndicator;
+    [SerializeField] public GameObject rangeIndicator;
+    private bool holdingDistraction = false;
+    [SerializeField] TextMeshProUGUI distractionsLeftText;
+    public int distractionsAmount = 5;
+    private GameObject currDistraction;
+    private GameObject currRangeIndicator;
     //TODO: Cooldown for using distraction/limiting it to only one placed at a time (If we want to handle more, change MonsterMovement.cs is neccessary)
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse1))
+        if(Input.GetKeyDown(KeyCode.Mouse1) && holdingDistraction)
         {
             Ray ray = topDownView.ScreenPointToRay(Input.mousePosition); //when we rightclick, shoot a ray from the topView-camera plane to where the user clicked
 
@@ -18,8 +25,40 @@ public class PlaceDistraction : MonoBehaviour
             {
                 int x = Mathf.RoundToInt(hit.point.x);
                 int z = Mathf.RoundToInt(hit.point.z);
-                Instantiate(distraction, new Vector3(x, 0.2f, z), Quaternion.identity);
+                
+                if(x >= 0 && z >= 0 && x < 10 && z < 10) //in bounds of the maze, current boundaries hardcoded
+                {
+                    Instantiate(distraction, new Vector3(x, 0.2f, z), Quaternion.identity);
+                    Destroy(currDistraction);
+                    Destroy(currRangeIndicator);
+                    distractionsAmount -= 1;
+                    distractionsLeftText.text = "Distractions left: " + distractionsAmount;
+                    holdingDistraction = false;
+                }
             }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Mouse1) && !holdingDistraction && !GameObject.FindWithTag("distraction") && distractionsAmount > 0)
+        {
+            holdingDistraction = true;
+            Ray ray = topDownView.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out RaycastHit hit, 100))
+            {
+                int x = Mathf.RoundToInt(hit.point.x);
+                int z = Mathf.RoundToInt(hit.point.z);
+
+                currDistraction = Instantiate(distractionIndicator, new Vector3(x, 0.2f, z), Quaternion.identity);
+                currRangeIndicator = Instantiate(rangeIndicator, new Vector3(x, 0f, z), Quaternion.identity);
+            }
+        }
+
+        if(currDistraction && currRangeIndicator)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos = topDownView.ScreenToWorldPoint(mousePos);
+            currDistraction.transform.position = new Vector3(mousePos.x, 0.2f, mousePos.z);
+            currRangeIndicator.transform.position = new Vector3(mousePos.x, 0, mousePos.z);
         }
     }
 }
