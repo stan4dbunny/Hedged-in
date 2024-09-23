445d6b50c924916aca59cf11fd4334e3a2e8dd7c
Heliuhm <helinsae@gmail.com>
2024-09-23 15:28:29 +0200

    door sound
    plays a sound when the door is opening/closing, also adjusted the rotation speed

--- Assets/Scripts/WallController.cs
+++ Assets/Scripts/WallController.cs
@@ -6,15 +6,29 @@ public class WallController : MonoBehaviour
 {
     private bool isRotating = false;
     private bool rotateForward = true;
-    private float rotationSpeed = 500f;
+    private float rotationSpeed = 100f;
     private float rotationAngle = 90f;
     private Quaternion targetRotation;
+
+    private AudioSource audioSource;
+    public AudioClip doorClip;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false;
+        }
+    }
 
     void Update()
     {
         if (isRotating)
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            PlayDoorSound();
             if (Quaternion.Angle(transform.rotation, targetRotation) < 0.01f)
             {
                 isRotating = false;
@@ -31,6 +45,15 @@ public class WallController : MonoBehaviour
             targetRotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, direction, 0));
             rotateForward = !rotateForward;
             isRotating = true;
+        }
+    }
+
+    private void PlayDoorSound()
+    {
+        if (doorClip != null && !audioSource.isPlaying)
+        {
+            audioSource.pitch = 1.8f;
+            audioSource.PlayOneShot(doorClip); 
         }
     }
 }
