5652e187a019493844ea0cac53740e8fe0581958
Carl Holmqvist <caholmq@kth.se>
2024-10-14 11:55:32 +0200

    user can no longer look into geometry, possible fix to mitigate camera drift, but need to discuss final solution with group

--- Assets/Scripts/VR/SetLocalPosition.cs
+++ Assets/Scripts/VR/SetLocalPosition.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class SetLocalPosition : MonoBehaviour
 {
+    public GameObject target;
+    public Vector3 manualPos;
+
     void Update()
     {
-        transform.localPosition = new Vector3(0, 0, 0);
-        transform.localRotation = Quaternion.identity;
+        if(target == null)
+        {
+            transform.localPosition = manualPos;
+            transform.localRotation = Quaternion.identity;
+        }
+        else
+        {
+            switch(gameObject.tag)
+            {
+                case "playerModel":
+                    transform.localPosition = new Vector3(target.transform.localPosition.x, -0.87f, target.transform.localPosition.z);
+                    Vector3 targetRot = target.transform.eulerAngles;
+                    transform.localRotation = Quaternion.Euler(0, targetRot.y, 0);
+                    break;
+                default:
+                    transform.localPosition = target.transform.localPosition;
+                    transform.localRotation = Quaternion.identity;
+                    break;
+            } 
+        } 
     }
 }
